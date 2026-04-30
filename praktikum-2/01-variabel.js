357f47e152d0c97b9036f94ebfa2767b62409b3b
sayyidalkautsar-ux <sayedalkausar@gmail.com>
2026-04-30 08:58:35 +0700

    Add files via upload

---
+++ praktikum-2/01-variabel.js
@@ -0,0 +1,28 @@
+//01-variabel.js
+//=============================
+//VARIABEL DAN DEKLARASI
+//=============================
+
+//--- Deklarasi dengan let (nilai bisa diubah) ---
+let nama = 'Budi';
+let umur = 20;
+let kota = 'Banda Aceh';
+
+console.log('=== Data Mahasiswa ===');
+console.log('Nama :', nama);
+console.log('Umur :', umur);
+console.log('Kota :', kota);
+
+//--- Mengubah nilai let ---
+umur = 21;
+console.log('Ulang tahun! Umur sekarang:', umur);
+
+//--- Deklarasi dengan const (nilai TIDAK bisa diubah) ---
+const jurusan = 'Teknik Informatika';
+const tahunmasuk = 2023;
+
+console.log('jurusan    :', jurusan);
+console.log('tahun masuk:', tahunmasuk);
+
+//--- coba hapus '//' di baris bawah ini, lalu jalankan ulang ---
+// jurusan = 'Sistem Informasi'; //--> ini akan menyebabkan ERROR!
---
+++ praktikum-2/tugas/tugas-2.js
@@ -0,0 +1,24 @@
+// deklarasi variabel 
+const beratBadan = 68;     // dalam kg
+const tinggiBadan = 1.72;  // dalam meter
+
+// hitung BMI 
+let bmi = beratBadan / (tinggiBadan * tinggiBadan);
+
+// ambil 2 angka desimal
+let bmiFix = bmi.toFixed(2);
+
+//menentukan kategori 
+let kategori = "";
+
+if (bmi < 18.5) {
+    kategori = "Kurus (Underweight)";
+}else if (bmi >= 18.5 && bmi <= 24.9) {
+    kategori = "Normal (ideal)";
+}else if (bmi >= 25.0 && bmi <= 29.9) {
+    kategori = "Kelebihan Berat Badan (Overweight)";
+}else {
+    kategori = "Obesitas (Obese)";
+}
+// tampilan hasil
+console.log(`Berat: ${beratBadan} kg | Tinggi: ${tinggiBadan} m | BMI: ${bmiFix} | Kategori: ${kategori}`);
